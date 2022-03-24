ee80e83d1b9a7e895eab1fb55d2021dc96496220
mogamoga <mogamoga.g1thu6@gmail.com>
2022-03-25 02:40:12 +0900

    設定 一括

--- js/viewModel/settingsViewModel.js
+++ js/viewModel/settingsViewModel.js
@@ -3,7 +3,22 @@ Vue.component("settings", {
     template: 
         `<div id="gojuon">
             <table>
-                <tr v-for="gojuonRow in gojuonList">
+                <tr>
+                    <td class="clear right-arrow-width"></td>
+                    <td class="clear border-bottom" v-for="n in 5">
+                        <div
+                            class="down-arrow-button"
+                            @click="colCharActiveSwitch(n - 1)"
+                        ></div>
+                    </td>
+                </tr>
+                <tr v-for="(gojuonRow, index) in gojuonList">
+                    <td class="clear border-right right-arrow-width">
+                        <div 
+                            class="right-arrow-button"
+                            @click="rowCharActiveSwitch(index)"
+                        ></div>
+                        </td>
                     <template v-for="char in gojuonRow">
                         <td v-if="char === null" class="unselectable"></td>
                         <td v-else
@@ -22,6 +37,18 @@ Vue.component("settings", {
     methods: {
         charActiveSwitch: function(char) {
             char.isSelected = !char.isSelected;
+        },
+        rowCharActiveSwitch: function(index) {
+            const isSelected = this.gojuonList[index].filter(c => c !== null && c.isSelected).length === 0;
+            this.gojuonList[index].forEach(c => {
+                if (c !== null) c.isSelected = isSelected;
+            });
+        },
+        colCharActiveSwitch: function(index) {
+            const isSelected = this.gojuonList.filter(row => row[index] !== null && row[index].isSelected).length === 0;
+            this.gojuonList.forEach(row => {
+                if (row[index] !== null) row[index].isSelected = isSelected;
+            });
         }
     },
     mounted() {
